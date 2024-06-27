5eed4758292dc399d62b9fca6ecfe5628e662e24
kolafish <yujin.yujin@gmail.com>
2024-06-27 16:55:18 +0800

    orgnize the dir

---
+++ WeSplit/proj2/button.swift
@@ -0,0 +1,18 @@
+//
+//  button.swift
+//  WeSplit
+//
+//  Created by Kola Fish on 2024/6/27.
+//
+
+import SwiftUI
+
+struct button: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    button()
+}
