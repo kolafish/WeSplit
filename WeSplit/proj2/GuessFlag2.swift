1905aaac10ada511eec741acca44e942ed7c7f7e
kolafish <yujin.yujin@gmail.com>
2024-06-27 23:02:33 +0800

    add the guess the flag

---
+++ WeSplit/proj2/GuessFlag2.swift
@@ -0,0 +1,18 @@
+//
+//  GuessFlag2.swift
+//  WeSplit
+//
+//  Created by Kola on 2024/6/27.
+//
+
+import SwiftUI
+
+struct GuessFlag2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GuessFlag2()
+}
