b67aecbde6800aaae0c254ffc8cafd413d8db38a
kolafish <yujin.yujin@gmail.com>
2024-07-02 19:59:24 +0800

    finish proj8 challenge 3

--- proj8/Moonshot.swift
+++ proj8/Moonshot.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct Moonshot: View {
+    @State var isList = false
     let astronauts : [String: Astronaut] = Bundle.main.decode("astronauts.json")
     let missions: [Mission] = Bundle.main.decode("missions.json")
     let columns = [
@@ -19,50 +20,72 @@ struct Moonshot: View {
     
     var body: some View {
         NavigationStack {
-            ScrollView {
-                LazyVGrid(columns: columns) {
-                    ForEach(missions) { mission in
-                        NavigationLink {
-                            MissionView(mission: mission, astronauts: astronauts)
-                        } label: {
-                            VStack {
-                                Image(mission.image)
-                                    .resizable()
-                                    .scaledToFit()
-                                    .frame(width: 100, height: 100)
-                                    .padding()
-                                VStack {
-                                    Text(mission.displayName)
-                                        .font(.headline)
-                                        .foregroundStyle(.white)
-                                    Text(mission.formattedLaunchDate)
-                                        .font(.caption)
-                                        .foregroundStyle(.white.opacity(0.5))
 
-                                }
-                                .padding(.vertical)
-                                .frame(maxWidth: .infinity)
-                                .background(.lightBackground)
+
+                Group {
+                    if isList {
+                        ScrollView {
+                            LazyVGrid(columns: columns) {
+                                myContent(m:self)
                             }
-                            .clipShape(.rect(cornerRadius: 10))
-                            .overlay(
-                                RoundedRectangle(cornerRadius: 10)
-                                    .stroke(.lightBackground)
-                            )
+                        }
+                        .padding([.horizontal, .bottom])
+                    } else {
+                        List {
+                            myContent(m : self)
                         }
                     }
                 }
-                .padding([.horizontal, .bottom])
-            }
             .navigationTitle("Moonshot")
             .background(.darkBackground)
             .preferredColorScheme(.dark)
             .toolbar {
-                EditButton()
+                Button("Add Expense", systemImage: "list.bullet") {
+                    isList.toggle()
+                }
             }
+
 
         }
 
+    }
+    
+    struct myContent: View {
+        var  m : Moonshot
+        var body: some View {
+ 
+            ForEach(m.missions) { mission in
+                    NavigationLink {
+                        MissionView(mission: mission, astronauts: m.astronauts)
+                    } label: {
+                        VStack {
+                            Image(mission.image)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 100, height: 100)
+                                .padding()
+                            VStack {
+                                Text(mission.displayName)
+                                    .font(.headline)
+                                    .foregroundStyle(.white)
+                                Text(mission.formattedLaunchDate)
+                                    .font(.caption)
+                                    .foregroundStyle(.white.opacity(0.5))
+
+                            }
+                            .padding(.vertical)
+                            .frame(maxWidth: .infinity)
+                            .background(.lightBackground)
+                        }
+                        .clipShape(.rect(cornerRadius: 10))
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(.lightBackground)
+                        )
+                    }
+                }
+            }
+        
     }
 }
 
