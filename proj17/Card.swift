189a7c5fd0fc0dce69a4a11c60b23ef357fb823c
kolafish <yujin.yujin@gmail.com>
2024-07-17 23:43:57 +0800

    finish proj17_3

---
+++ proj17/Card.swift
@@ -0,0 +1,15 @@
+//
+//  Card.swift
+//  WeSplit
+//
+//  Created by Kola Fish on 2024/7/17.
+//
+
+import Foundation
+
+struct Card {
+    var prompt: String
+    var answer: String
+
+    static let example = Card(prompt: "Who played the 13th Doctor in Doctor Who?", answer: "Jodie Whittaker")
+}
---
+++ proj17/CardView.swift
@@ -0,0 +1,65 @@
+//
+//  CardView.swift
+//  WeSplit
+//
+//  Created by Kola Fish on 2024/7/17.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    let card: Card
+    @State private var isShowingAnswer = false
+    @State private var offset = CGSize.zero
+    var removal: (() -> Void)? = nil
+
+
+
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 25)
+                .fill(.white)
+                .shadow(radius: 10)
+
+
+            VStack {
+                Text(card.prompt)
+                    .font(.largeTitle)
+                    .foregroundStyle(.black)
+
+                if isShowingAnswer {
+                    Text(card.answer)
+                        .font(.title)
+                        .foregroundStyle(.secondary)
+                }
+            }
+            .padding(20)
+            .multilineTextAlignment(.center)
+        }
+        .frame(width: 450, height: 250)
+        .rotationEffect(.degrees(offset.width / 5.0))
+        .offset(x: offset.width * 5)
+        .opacity(2 - Double(abs(offset.width / 50)))
+        .gesture(
+            DragGesture()
+                .onChanged { gesture in
+                    offset = gesture.translation
+                }
+                .onEnded { _ in
+                    if abs(offset.width) > 100 {
+                        
+                        removal?()
+                    } else {
+                        offset = .zero
+                    }
+                }
+        )
+        .onTapGesture {
+            isShowingAnswer.toggle()
+        }
+    }
+}
+
+#Preview {
+    CardView(card : .example)
+}
